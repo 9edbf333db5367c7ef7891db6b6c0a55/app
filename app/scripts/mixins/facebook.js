1c04893e0d205341b0cd914b38436077457a7079
Eugene Mutai <eugenemutai@gmail.com>
2017-04-25 11:38:44 +0300

    Minor refactor

--- app/scripts/mixins/facebook.js
+++ app/scripts/mixins/facebook.js
@@ -12,19 +12,19 @@ export default {
           'response_type=token&',
           // 'state=SomeSuperSecretCodeIUseToCubCSRF',
           `scope=${this.facebook.scopes.join(',')}`,
-        ].join('');
+        ];
 
-        const options = [url, '_blank', this.inAppBrowserOptions];
+        const options = [url.join(''), '_blank', this.inAppBrowserOptions];
         const browser = cordova.ThemeableBrowser.open(...options);
         browser.addEventListener('loadstop', event => {
           if (event.url.indexOf('#access_token') > -1) {
             const params = querystring.parse(event.url.replace('?#', '?').split('?')[1]);
             console.log('ACCESS_TOKEN', params.access_token);
-            browser.close();
 
             // Build Firebase credential with the Facebook access token.
             const credential = firebase.auth.FacebookAuthProvider.credential(params.access_token);
             resolve(firebase.auth().signInWithCredential(credential));
+            browser.close();
           }
         });
       });
