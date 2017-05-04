01d0d018e83eb636d702370e9a56e12015341b3b
Eugene Mutai <eugenemutai@gmail.com>
2017-05-04 13:15:34 +0300

    Removed crosswalk from build

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,7 +9,8 @@ cordova plugin add cordova-plugin-themeablebrowser
 cordova plugin add cordova-plugin-network-information
 cordova plugin add cordova-plugin-splashscreen
 cordova plugin add cordova-plugin-statusbar
-cordova plugin add cordova-plugin-crosswalk-webview
+cordova plugin add cordova-plugin-geolocation --variable GEOLOCATION_USAGE_DESCRIPTION="Knowing your location will enable VituMob to precisely identify where to deliver your order"
+# cordova plugin add cordova-plugin-crosswalk-webview
 cd ..
 gulp clean
 bower install
